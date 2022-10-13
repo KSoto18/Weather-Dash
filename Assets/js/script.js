370306c7aac35886f8648c7373e36b5b2b410ce0
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-13 17:04:03 -0400

    tried to fix some bugs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -89,7 +89,7 @@ fetch(weatherAPI, {
 });
 
   for (var i = 0; i < localStorage.length; i++) {
-    $("#search-history").append("<p>" + localStorage.getItem(localStorage.key(i)) + "</p>");
+    $("#search-history").append("<btn>" + localStorage.getItem(localStorage.key(i)) + "</btn>");
   }
 }
 
